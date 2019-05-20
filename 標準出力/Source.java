cda36d15156c473cb15f12bdc47d4182c8ab6634
yoneya1014 <yoneya1014@gmail.com>
2019-05-20 09:40:30 +0900

    標準出力

---
+++ 標準出力/Source.java
@@ -0,0 +1,7 @@
+public class Source{
+    public static void main(String[] args){
+        System.out.println("テキスト表示(改行あり)");
+        System.out.print("テキスト表示(改行なし)");
+        System.out.printf("変数表示(整数)：%d\n", 1);
+    }
+}
